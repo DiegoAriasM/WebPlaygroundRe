6a1bbe00ae5334a757a859ba50ee992f3b662556
Diego Arias <jack12diego@gmail.com>
2020-08-08 21:05:51 -0500

    Novena App Profiles

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -17,15 +17,20 @@ from django.contrib import admin
 from django.urls import path, include 
 from pages.urls import pages_patterns
 from django.conf import settings
+from profiles.urls import profiles_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+   
 
     #paths de auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+
+    #paths de profiles
+    path('profiles/', include(profiles_patterns)),
 ]
 
 if settings.DEBUG:
