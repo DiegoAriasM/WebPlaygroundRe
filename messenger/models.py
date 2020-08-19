cc7cc4488f5e01b8e47ed21c3f260f89ced127c1
Diego Arias <jack12diego@gmail.com>
2020-08-19 07:28:34 -0500

    final

--- messenger/models.py
+++ messenger/models.py
@@ -28,8 +28,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta: 
+        ordering = ['-updated']
 
 
 def messages_changed(sender, **kwargs):
@@ -47,6 +51,8 @@ def messages_changed(sender, **kwargs):
                 false_pk_set.add(msg_pk)
 
     pk_set.difference_update(false_pk_set)
+
+    instance.save()
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
 
