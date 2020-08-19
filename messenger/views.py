79e1e71f8c69fb18c42e08f1518f80bcbe52e0a6
Diego Arias <jack12diego@gmail.com>
2020-08-19 05:12:18 -0500

    mensajes asíncronos

--- messenger/views.py
+++ messenger/views.py
@@ -3,22 +3,12 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from .models import Thread
-from django.http import Http404
-<<<<<<< HEAD
-=======
-
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
->>>>>>> ce65623273b556746ccc40492f20886a80804799
+from django.http import Http404, JsonResponse
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 # Create your views here.
-<<<<<<< HEAD
 @method_decorator(login_required, name="dispatch")
-=======
-@method_decorator(login_required, name="disparch")
->>>>>>> ce65623273b556746ccc40492f20886a80804799
 class ThreadList(TemplateView):
     template_name = "messenger/thread_list.html"
 
@@ -29,10 +19,10 @@ class ThreadDetail(DetailView):
     def get_object(self):
         obj = super(ThreadDetail, self).get_object()
         if self.request.user not in obj.users.all():
-<<<<<<< HEAD
             raise Http404()
         return obj
-=======
-            raise Http404
-        return obj
->>>>>>> ce65623273b556746ccc40492f20886a80804799
+
+def add_message(request, pk):
+    print(request.GET)
+    json_response = {'created':False}
+    return JsonResponse(json_response)
